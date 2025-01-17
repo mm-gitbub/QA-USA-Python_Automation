3a9297b0595a9421ee81a9db29d30428923fb169
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-01-16 23:45:57 -0500

    Second commit

--- data.py
+++ data.py
@@ -1,1 +1,13 @@
 # File with constants
+# This constant is used to store the URL from start server button.
+
+URBAN_ROUTES_URL = 'https://cnt-a9345205-f1c9-4ad2-bed7-a719a6f46e61.containerhub.tripleten-services.com'
+
+# The name and the test data as the value for each of these:
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,50 @@
+import data
+import helpers
+
+#from helpers import is_url_reachable
+#from data import URBAN_ROUTES_URL
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Task 4: Check the Server is on.
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    # Task 3: Define 8 functions
+    def test_set_route(self):
+        # Add in S8
+        print("function created for setting route")
+        pass
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for selecting plan")
+        pass
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for filling phone number")
+        pass
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for filling the card information")
+        pass
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment to driver")
+        pass
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for ordering blanket and handkerchief")
+        pass
+    # Task 5: Preparing the ice cream order
+    def test_order_2_ice_creams(self):
+        print("function created for ordering 2 ice creams")
+        for i in range(2):
+            # Add in S8
+            pass
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for searching car model")
+        pass
